894d0906a9abf64622944d7ecbc836eeacef24c5
Els <Emathivannan@spartaglobal.com>
2018-08-14 10:10:14 +0100

    Fixed indentation and added more comments for clarification

--- js/main.js
+++ js/main.js
@@ -2,14 +2,10 @@ $(function(event){
   //Reference to the html element
   var canvas = document.querySelector("#canvas");
   var context = canvas.getContext('2d');
-  var context1 = canvas.getContext('2d');
-
+  //Set a variable for movement along x and y axis
   var xPos = 0;
   var yPos = 0;
-
-  var xPos1 = 0;
-  var yPos1 = 0;
-
+  //function which initially draws the triangle and whenever its moved by the arrow key
   function drawTriangle() {
     //This ensures we clear the code out before attempting to re-draw the triangle
     context.clearRect(0, 0, canvas.width, canvas.height)
@@ -27,7 +23,6 @@ $(function(event){
     context.fillStyle = "rgba(255, 204, 0, 1)"
     context.fill();
   }
-
   //call function
   drawTriangle();
   // drawTriangle1();
@@ -36,43 +31,49 @@ $(function(event){
   window.addEventListener("keyup", keysReleased, false);
   //Set an array to store multiple key inputs
   var keys = []
-
+  //function for when the the key which moves the character is pressed
   function keysPressed(e){
     //store an entry for every key pressed
     keys[e.keyCode] = true;
       //player yellow triangle
       //left key pressed
-      if (keys[37]){
-        xPos -= 10;
-        if (xPos <= -170){
-          xPos += 10;
-        }
-      }
-      //right key pressed
-      if (keys[39]){
+    if (keys[37]){
+      xPos -= 10;
+      //if the triangle reaches the edge of the canvas, this will make it stop
+      if (xPos <= -170){
         xPos += 10;
-        if (xPos >= 370){
-          xPos -= 10;
-        }
       }
-      //up key pressed
-      if (keys[38]){
-        yPos -= 10;
-        if (yPos <= -100){
-          yPos += 10;
-        }
+    }
+    //right key pressed
+    if (keys[39]){
+      xPos += 10;
+      //if the triangle reaches the edge of the canvas, this will make it stop
+      if (xPos >= 370){
+        xPos -= 10;
       }
-      // down key pressed
-      if (keys[40]){
+    }
+    //up key pressed
+    if (keys[38]){
+      yPos -= 10;
+      //if the triangle reaches the edge of the canvas, this will make it stop
+      if (yPos <= -100){
         yPos += 10;
-        if (yPos >= 450){
-          yPos -= 10;
-        }
       }
+    }
+    // down key pressed
+    if (keys[40]){
+      yPos += 10;
+      //if the triangle reaches the edge of the canvas, this will make it stop
+      if (yPos >= 450){
+        yPos -= 10;
+      }
+    }
     //Stop the page from using the default input of keyboard inputs
     e.preventDefault()
+    // calls the drawTriangle function
     drawTriangle();
   }
+  //The funciton which states the key pressed as false when it is released
   function keysReleased(e){
     //mark keys that are keysReleased
     keys[e.keyCode] = false;
