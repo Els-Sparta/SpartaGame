da776768ab8c556423a2b146496beb1d17e543bb
Els <Emathivannan@spartaglobal.com>
2018-08-14 08:49:22 +0100

    Added comments

--- js/main.js
+++ js/main.js
@@ -28,23 +28,6 @@ $(function(event){
     context.fill();
   }
 
-  // function drawTriangle1() {
-  //   //This ensures we clear the code out before attempting to re-draw the triangle
-  //   context1.clearRect(0, 0, canvas.width, canvas.height)
-  //   //the character
-  //   context1.beginPath();
-  //   context1.moveTo(300 + xPos1, 200 + yPos1);
-  //   context1.lineTo(170 + xPos1, 150 + yPos1);
-  //   context1.lineTo(230 + xPos1, 150 + yPos1);
-  //   context1.closePath();
-  //   //outline
-  //   context1.lineWidth = 10;
-  //   context1.strokStyle = "rgba(102, 102, 102, 1)"
-  //   context1.stroke();
-  //   //fill the shape
-  //   context1.fillStyle = "rgba(255, 204, 0, 1)"
-  //   context1.fill();
-  // }
   //call function
   drawTriangle();
   // drawTriangle1();
@@ -94,54 +77,4 @@ $(function(event){
     //mark keys that are keysReleased
     keys[e.keyCode] = false;
   }
-
 })
-// var xPos1 = 0;
-// var yPos1 = 0;
-//
-// var xPos2 = 70;
-// var yPos2 = 0;
-//
-// context2.rect(xPos2, yPos2, 50, 50);
-// context2.stroke();
-//
-// context1.rect(xPos1, yPos1, 50, 50);
-// context1.stroke();
-//
-// function move(e){
-//   // alert(e.keyCode);
-//
-//   if(e.keyCode==39){
-//     xPos1+=15;
-//   }
-//   if(e.keyCode==37){
-//     xPos1-=15;
-//   }
-//   if(e.keyCode==38){
-//     yPos1-=15;
-//   }
-//   if(e.keyCode==40){
-//     yPos1+=15;
-//   }
-//
-//   if(e.keyCode==68){
-//     xPos2+=15;
-//   }
-//   if(e.keyCode==65){
-//     xPos2-=15;
-//   }
-//   if(e.keyCode==87){
-//     yPos2-=15;
-//   }
-//   if(e.keyCode==83){
-//     yPos2+=15;
-//   }
-//
-//   canvas.width=canvas.width;
-//   context1.rect(xPos1, yPos1, 50, 50);
-//   context1.stroke();
-//
-//   context2.rect(xPos2, yPos2, 50, 50);
-//   context2.stroke();
-// }
-//   document.onkeydown = move;
